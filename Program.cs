36f5893672e92cf80de160de2d16c29bcfb0706e
Herz <Herzus>
2020-02-04 04:20:29 +0100

    Parallelforeach

--- Program.cs
+++ Program.cs
@@ -1,19 +1,48 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace cSharpProves
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        internal static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+
+            int counter = 0;  
+            string line;  
             
             ReadFile readFile = new ReadFile();
 
             string path = @"c:\test\2020\";
 
-            readFile.OpenFile(path); 
+            readFile.OpenFile(path);
+
+            string[] files = readFile.ListFile.ToArray(); 
+
+            Parallel.ForEach(files,(currentFile) => {
+                System.Console.WriteLine(currentFile);
+                
+                System.IO.StreamReader file = new System.IO.StreamReader(currentFile);
+
+             
+                while((line = file.ReadLine()) != null)  
+                {  
+                    System.Console.WriteLine(line);  
+                    counter++;  
+                }  
+  
+                file.Close();  
+                System.Console.WriteLine("There were {0} lines.", counter);  
+                // Suspend the screen.  
+                //System.Console.ReadLine();  
+            });
+
             
+          
+ 
             
         }
     }
--- ReadFile.cs
+++ ReadFile.cs
@@ -1,10 +1,13 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 class ReadFile
 
 {
+    public List<string> ListFile = new List<string>();
+
     public void OpenFile(String path)
     {
         
@@ -14,9 +17,11 @@ class ReadFile
         //System.IO.File.ReadAllText(@"C:\test\2020\test.txt");
 
         //System.Console.WriteLine("te = " + file.Replace("prova","hola"));
-        foreach(string fileName in fileNames)
-            System.Console.WriteLine(fileName);
+        foreach(string fileName in fileNames){
+            //System.Console.WriteLine(fileName);
+            ListFile.Add(fileName);
 
+        }
 
         string[] subdirectoryEntries = Directory.GetDirectories(path);
             foreach(string subdirectory in subdirectoryEntries)
